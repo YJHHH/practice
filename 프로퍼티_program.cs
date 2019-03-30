fc84e724e9e1d53e929b12a8b6941520b551b269
8berry <wmfwmf5254@gmail.com>
2019-03-31 03:11:38 +0900

    Create 프로퍼티_program.cs

---
+++ 프로퍼티_program.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class program : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Salary mySalary = new Salary();
+   
+    void Start()
+    {
+        int salary = mySalary.SalaryP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
